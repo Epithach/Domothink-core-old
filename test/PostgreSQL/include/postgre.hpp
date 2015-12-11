7ad31b7cba3d68690dbc9435f3d15f18db4743b3
thach_b <thach_b@epitech.eu>
2015-12-11 13:53:58 +0100

    Adding Postgre test

---
+++ test/PostgreSQL/include/postgre.hpp
@@ -0,0 +1,35 @@
+#ifndef		POSTGRE_HPP_
+# define	POSTGRE_HPP_
+
+#include <iostream>
+#include <ostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+//#include <libpq-fe.h>
+
+class		Postgre {
+private:
+  char		*DbName_;
+  char		*User_;
+  char		*Passwd_;
+  char		*Ip_;
+  int		Port_;
+public:
+  Postgre(char *, char *, char *, char *, char *);
+  ~Postgre();
+
+  void		run();
+  bool		setDbName(char *);
+  char		*getDbName() const;
+  bool		setUser(char *);
+  char		*getUser() const;
+  bool		setPasswd(char *);
+  char		*getPasswd() const;
+  bool		setIp(char *);
+  char		*getIp() const;
+  void		setPort(int);
+  int		getPort() const;
+};
+
+#endif		/* !POSTGRE_HPP_ */
---
+++ test/PostgreSQL/src/postgre.cpp
@@ -0,0 +1,92 @@
+#include "../include/postgre.hpp"
+
+Postgre::Postgre(char *dbname, char *user, char *password, char *ip, char *port) {
+
+
+  if (setDbName(dbname) == false)
+    throw ;
+  std::cout << "DbName : " <<this->DbName_ << std::endl;
+  if (setUser(user) == false)
+    throw ;
+  std::cout << "User : " <<this->User_ << std::endl;
+  if (setPasswd(password) == false)
+    throw ;
+  std::cout << "Passwd : " <<this->Passwd_ << std::endl;
+  if (setIp(ip) == false)
+    throw ;
+  std::cout << "Ip : " <<this->Ip_ << std::endl;
+  setPort(atoi(port));
+  std::cout << "Port : " <<this->Port_ << std::endl;
+
+  std::cout << "CONSTRUCTOR OK" << std;;endl;
+}
+
+
+Postgre::~Postgre() {
+  
+  free(this->DbName_);
+  free(this->User_);
+  free(this->Passwd_);
+  free(this->Ip_);
+  free(this->Port_);
+
+  
+}
+
+void		Postgre::run() {
+
+  std::cout << "POSTGRE GO >>>" << std::endl;
+
+  return ;
+}
+bool		Postgre::setDbName(char *db) {
+  if ((this->DbName_ = (char *)malloc(sizeof(char *) * 4096)) == NULL)
+    return false;
+  strcpy(this->DbName_, db);
+  return true;
+}
+
+char		*Postgre::getDbName() const  {
+  return this->DbName_;
+}
+
+bool		Postgre::setUser(char *user) {
+  if ((this->User_ = (char *)malloc(sizeof(char *) * 4096)) == NULL)
+    return false;
+  strcpy(this->User_, user);
+  return true;
+}
+
+char		*Postgre::getUser() const {
+  return this->User_;
+}
+
+bool		Postgre::setPasswd(char *pass) {
+  if ((this->Passwd_ = (char *)malloc(sizeof(char *) * 4096)) == NULL)
+    return false;
+  strcpy(this->Passwd_, pass);
+  return true;
+}
+
+char		*Postgre::getPasswd() const {
+  return this->Passwd_;
+}
+
+bool		Postgre::setIp(char *ip) {
+  if ((this->Ip_ = (char *)malloc(sizeof(char *) * 4096)) == NULL)
+    return false;
+  strcpy(this->Ip_, ip);
+  return true;
+}
+
+char		*Postgre::getIp() const {
+  return this->Ip_;
+}
+
+void		Postgre::setPort(int port) {
+  this->Port_ = port;
+}
+
+int		Postgre::getPort() const {
+  return this->Port_;
+}
